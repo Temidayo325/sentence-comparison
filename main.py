7e29e6a7e75afdcc29a7603cd9d876a86ecda067
Temidayo325 <59534757+Temidayo325@users.noreply.github.com>
2023-01-05 03:49:55 +0100

    single statement(e.g definition) comparison endpoint working well

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from sentence_transformers import SentenceTransformer, util
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 app = FastAPI()
 
@@ -7,6 +10,14 @@ class Sentence(BaseModel):
     scheme: str
     answer: str
     mark: int
+
 @app.post('/compare/explaination')
 def compareExplaination(sentence: Sentence):
-    return sentence
+    tryToMark : int = 0
+    scheme_embedding = model.encode(sentence.scheme, convert_to_tensor=True)
+    answer_embedding = model.encode(sentence.answer, convert_to_tensor=True)
+    cosine_scores = util.cos_sim(scheme_embedding, answer_embedding)
+    for index in cosine_scores:
+        for value in index:
+            tryToMark = float(value) * sentence.mark
+    return {"scheme": sentence.scheme, "answer": sentence.answer, "score": tryToMark}
