e4a06b750273b4e33fea6f90180af20666b5867f
Temidayo325 <59534757+Temidayo325@users.noreply.github.com>
2023-01-09 17:04:33 +0100

    list comparison method added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 app = FastAPI()
 
+
 class Sentence(BaseModel):
     scheme: str
     answer: str
@@ -20,4 +21,25 @@ def compareExplaination(sentence: Sentence):
     for index in cosine_scores:
         for value in index:
             tryToMark = float(value) * sentence.mark
-    return {"scheme": sentence.scheme, "answer": sentence.answer, "score": tryToMark}
+    return {"score": tryToMark}
+
+
+class Listed(BaseModel):
+    scheme: list
+    answer: list
+    mark: int
+@app.post('/compare/list')
+def compareList(sentence: Listed):
+    destring_scheme: list = sentence.scheme
+    destring_answer: list = sentence.answer
+    scheme_embedding = model.encode(destring_scheme, convert_to_tensor=True)
+    answer_embedding = model.encode(destring_answer, convert_to_tensor=True)
+    cosine_scores = util.cos_sim(scheme_embedding, answer_embedding)
+    values: list = []
+    final_values: int = 0
+    for answer in cosine_scores:
+        values.append(max(answer))
+    for final in destring_answer:
+        final_values += float(max(values) * int(sentence.mark))
+        values.remove(max(values))
+    return {"final_score": final_values}
