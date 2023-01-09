c1f749919ccb5320dab279c86c288738ad9777e6
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2023-01-09 08:11:01 +0100

    second refactor

--- router/main.py
+++ router/main.py
@@ -6,18 +6,26 @@ from dotenv import load_dotenv
 
 
 
-from services.sentencecompare import Comparison
+from services.sentencecompare import Sentencecomparison
+from services.listcompare import Listcomparison
 from fastapi import FastAPI, APIRouter
 
 load_dotenv('.env')
 
 app = FastAPI()
 
-compare = Comparison()
+compareSentence = Sentencecomparison()
+compareList = Listcomparison()
 
 
 router = APIRouter()
-router.add_api_route(os.getenv("COMPARE_EXPLANATION_ENDPOINT"), endpoint=compare.compareExplanation, methods=["POST"])
+
+router.add_api_route(os.getenv("COMPARE_EXPLANATION_ENDPOINT"), 
+endpoint = compareSentence.compareExplanation, methods=["POST"])
+
+router.add_api_route(os.getenv("COMPARE_LISTED_ENDPOINT"), 
+endpoint = compareList.compareList, methods=["POST"])
+
 app.include_router(router)
 
 
